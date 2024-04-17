aa5844f11995c3e400465920492092f1cb61b9c9
dgutper960 <dgutper960@outlook.es>
2024-04-17 08:36:07 +0200

    correccion errores

--- Fase3/src/app/detalle/detalle.page.ts
+++ Fase3/src/app/detalle/detalle.page.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute } from '@angular/router';
 import { Tarea } from '../tarea';
+import { FirestoreService } from '../firestore.service';
+
+interface Documento {
+  id: string;
+  data: Tarea;
+}
 
 @Component({
   selector: 'app-detalle',
@@ -8,31 +14,31 @@ import { Tarea } from '../tarea';
   styleUrls: ['./detalle.page.scss'],
 })
 export class DetallePage implements OnInit {
-
-  // Variable que almacena el id
   idTarea: string = "";
+  document: Documento = { id: "", data: {} as Tarea };
 
-  // Variable que recoge los datos de la consulta
-  // Necesitamos dos propiedades en la variable
-  document: any = {
-    id: "",
-    data: {} as Tarea // debemos importar Tarea
-  };
-
-  constructor(private activateRoute: ActivatedRoute) { 
-
-    
-
-  }
+  constructor(private firestoreService: FirestoreService, private activateRoute: ActivatedRoute) {}
 
   ngOnInit() {
-    // Almacenamos el id de la ruta en nuestra variable de la clase
-    // Usamos la instancia de ActivateRoute que hemos definido en el constructor
-    let idRecibido = this.activateRoute.snapshot.paramMap.get('id'); // mismo nombre usado en la ruta
-    // controlamos que el elemento, por lo que sea, no sea null
-    if(idRecibido != null){
+    let idRecibido = this.activateRoute.snapshot.paramMap.get('id');
+    if (idRecibido !== null) {
       this.idTarea = idRecibido;
+      this.consultarTarea();
     }
   }
 
+  consultarTarea() {
+    // hacemos consulta a la BBDD
+    this.firestoreService.consultarPorId("tareas", this.idTarea).subscribe((resultado:any)=>{
+      // En caso de obtener un document con ese nombre
+      if(resultado.payload.data() != null){
+        this.document.id = resultado.payload.id
+        this.document.data = resultado.payload.data();
+        // mostramos el titulo por consola
+        console.log(this.document.data.titulo);
+      }else{
+        this.document.data = {} as Tarea;
+      }
+    })
+  }
 }
